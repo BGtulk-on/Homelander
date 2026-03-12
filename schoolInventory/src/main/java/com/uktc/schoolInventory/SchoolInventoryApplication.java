7482f91c878959c5aca6c920189367f1b6cf6b5b
100yan <133891518+StoyanHarizanov@users.noreply.github.com>
2026-03-12 11:10:35 +0200

    Rnaming and adding spring init

---
+++ schoolInventory/src/main/java/com/uktc/schoolInventory/SchoolInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.uktc.schoolInventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolInventoryApplication.class, args);
+	}
+
+}
---
+++ schoolInventory/src/test/java/com/uktc/schoolInventory/SchoolInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uktc.schoolInventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
